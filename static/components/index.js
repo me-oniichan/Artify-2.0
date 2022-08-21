c0d87e7958dab3f01aea600267c0d1cf96bee616
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-08-21 18:13:23 +0530

    added buttons

--- static/components/index.js
+++ static/components/index.js
@@ -1,24 +1,29 @@
-const ImageArea = () =>{
-    return(
-        <img src="static/components/empty.jpg" alt="" />
+const ImageArea = () => {
+    return (
+        <div className="body">
+            <img id="image" src="static/components/empty.jpg" alt="" width="500px" />
+            <label htmlFor="file">Upload</label>
+            <input type="file" name="file" id="file" style={{ display: "none" }} accept="image/png, image/jpg, image/jpeg" />
+            <button className="btn convert">Convert</button>
+        </div>
     )
 }
 
-const Navbar =()=> {
+const Navbar = () => {
     return <div className="navbar">
-        Artify 
+        Artify
     </div>;
 }
 
-function App(){
-    return(
+function App() {
+    return (
         <React.Fragment>
-            <Navbar/>
-            <ImageArea/>
+            <Navbar />
+            <ImageArea />
         </React.Fragment>
     )
 }
 
 const domContainer = document.querySelector('#root');
 const root = ReactDOM.createRoot(domContainer);
-root.render(<App/>);
+root.render(<App />);

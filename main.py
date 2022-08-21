f50c1d4e627d126a64d5bd1f0c60311ec6d2c84f
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-08-22 00:20:01 +0530

    send image as response

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from uuid import uuid4
 
 import cv2
+import io
 import numpy as np
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from sklearn.cluster import KMeans
 
 app = Flask(__name__)
@@ -20,20 +21,18 @@ def home():
 def path():
     if request.method == 'POST':
         f = request.files["image"]
-        path = f"static/{str(uuid4())[:8]}.{f.filename.split('.')[-1]}"
-        f.save(path)
-        img = cv2.imread(path)
+        img = cv2.imdecode(np.frombuffer(f.stream.read(), dtype = np.uint8), cv2.IMREAD_UNCHANGED)
+
         img_norm = img.astype(np.float16)/255.0
         img_norm = np.reshape(
-            img_norm, (img_norm.shape[0]*img_norm.shape[1], 3))
+            img_norm, (img_norm.shape[0]*img_norm.shape[1], 4))
         kmean = KMeans(n_clusters=maxColor, max_iter=maxIter,
                        n_init=10).fit(img_norm)
         recoverd = (kmean.cluster_centers_*255).astype(np.uint8)
         recImg = (kmean.predict(img_norm))
         recImg = recoverd[recImg]
-        recImg = np.reshape(recImg, (img.shape[0], img.shape[1], 3))
-        cv2.imwrite(path, recImg)
-        return render_template("index.html", img=path)
+        recImg = np.reshape(recImg, img.shape)
+        return send_file(path_or_file = io.BytesIO(cv2.imencode("."+f.filename.split(".")[-1], recImg)[1]), mimetype="image/gif")
     return "Something not right.."
 
 
--- static/components/index.js
+++ static/components/index.js
@@ -1,19 +1,61 @@
 const ImageArea = () => {
+    const [source, setSource] = React.useState("static/components/empty.jpg");
+
+    let image = new Image();
+    image.src = source;
+    image.onload = function () {
+        let ratio = image.width / image.height;
+        if (image.height > 500) image.height = 500;
+        else if (image.width > 600) {
+            image.width = 600;
+            image.height = image.width / ratio;
+        }
+
+        document.getElementById("image").height = image.height;
+    };
+    const Upload = (e) => {
+        if (e.target.files[0])
+            setSource(URL.createObjectURL(e.target.files[0]));
+    };
+
+    const Convert = () => {
+        const formData = new FormData();
+        const imagefile = document.querySelector('#file');
+        formData.append("image", imagefile.files[0]);
+        axios.post('http://127.0.0.1:5000/image', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then(response=>{
+            console.log(response)
+        })
+
+    };
+
     return (
         <div className="body">
-            <img id="image" src="static/components/empty.jpg" alt="" width="500px" />
+            <img id="image" src={source} alt="" />
             <label htmlFor="file">Upload</label>
-            <input type="file" name="file" id="file" style={{ display: "none" }} accept="image/png, image/jpg, image/jpeg" />
-            <button className="btn convert">Convert</button>
+            <input
+                type="file"
+                name="file"
+                id="file"
+                style={{ display: "none" }}
+                accept="image/png, image/jpg, image/jpeg"
+                onChange={(e) => {
+                    Upload(e);
+                }}
+            />
+            <button className="btn" onClick={Convert}>
+                Convert
+            </button>
         </div>
-    )
-}
+    );
+};
 
 const Navbar = () => {
-    return <div className="navbar">
-        Artify
-    </div>;
-}
+    return <div className="navbar">Artify</div>;
+};
 
 function App() {
     return (
@@ -21,9 +63,9 @@ function App() {
             <Navbar />
             <ImageArea />
         </React.Fragment>
-    )
+    );
 }
 
-const domContainer = document.querySelector('#root');
+const domContainer = document.querySelector("#root");
 const root = ReactDOM.createRoot(domContainer);
 root.render(<App />);

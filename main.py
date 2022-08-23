9212e0423697763ec299565aee0df5bbef885631
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-08-23 23:24:01 +0530

    finished implementing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 import uuid
 import numpy as np
 from flask import Flask, render_template, request, send_file
@@ -6,7 +7,7 @@ from sklearn.cluster import KMeans
 
 app = Flask(__name__)
 maxIter = 10
-maxColor = 18
+maxColor = 20
 app.config['MAX_CONTENT_PATH'] = 2000000
 
 
@@ -15,11 +16,13 @@ def home():
     return render_template("index.html", img="static/empty.jpg")
 
 
-@app.route("/image", methods=["GET", "POST"])
-def path():
+@app.route("/image/<string:ext>", methods=["GET", "POST"])
+def path(ext):
     if request.method == 'POST':
         f = request.files["image"]
-        img = cv2.imdecode(np.frombuffer(f.stream.read(), dtype = np.uint8), cv2.IMREAD_UNCHANGED)
+
+        img = cv2.imdecode(np.frombuffer(
+            f.stream.read(), dtype=np.uint8), cv2.IMREAD_UNCHANGED)
 
         img_norm = img.astype(np.float16)/255.0
         img_norm = np.reshape(
@@ -30,16 +33,18 @@ def path():
         recImg = (kmean.predict(img_norm))
         recImg = recoverd[recImg]
         recImg = np.reshape(recImg, img.shape)
-        img_id = str(uuid.uuid4())[:8] + '.' + f.filename.split('.')[-1]
+        img_id = str(uuid.uuid4())[:8] + '.' + ext
         path = "static/Images/" + img_id
-        
         cv2.imwrite(path, recImg)
-        return path
+        return img_id
+    
     return "Something not right.."
 
-@app.route("/id/<string:imageid>")
-def getimage(imageid):
-    return
 
+@app.route("/remfromdisk", methods = ["POST"])
+def getimage():
+    os.remove("static/Images/" + request.json['path'])
+    return "success"
 
+    
 app.run(debug=True)
--- static/components/index.js
+++ static/components/index.js
@@ -1,36 +1,63 @@
 const ImageArea = () => {
-    const [source, setSource] = React.useState("static/components/empty.jpg");
+    const source = "static/components/empty.jpg";
 
-    let image = new Image();
-    image.src = source;
-    image.onload = function () {
-        let ratio = image.width / image.height;
-        if (image.height > 500) image.height = 500;
-        else if (image.width > 600) {
-            image.width = 600;
-            image.height = image.width / ratio;
-        }
+    let blob;
 
-        document.getElementById("image").height = image.height;
-    };
     const Upload = (e) => {
-        if (e.target.files[0])
-            setSource(URL.createObjectURL(e.target.files[0]));
+        if (!e.target.files[0]) return;
+
+        let image = new Image();
+        image.src = URL.createObjectURL(e.target.files[0]);
+        image.onload = function () {
+            let ratio = image.width / image.height;
+            if (image.height > 500) {
+                image.height = 500;
+                image.width = ratio * 500;
+            }
+            if (image.width > 800) {
+                image.width = 800;
+                image.height = image.width / ratio;
+            }
+
+            let canvas = document.createElement("canvas");
+            canvas.width = image.width;
+            canvas.height = image.height;
+            let ctx = canvas.getContext("2d");
+            ctx.drawImage(image, 0, 0, image.width, image.height);
+            canvas.toBlob((b) => {
+                let link = URL.createObjectURL(b);
+                document.getElementById("image").src = link;
+                blob = b;
+            });
+            URL.revokeObjectURL(image.src);
+        };
     };
 
     const Convert = () => {
         const formData = new FormData();
         let imagefile = document.querySelector("#file");
+        let ext = imagefile.files[0].name.split(".");
+        ext = ext[ext.length - 1];
+        formData.append("image", blob);
 
-        formData.append("image", imagefile.files[0]);
         axios
-            .post("http://127.0.0.1:5000/image", formData, {
+            .post("http://127.0.0.1:5000/image/" + ext, formData, {
                 headers: {
                     "Content-Type": "multipart/form-data",
                 },
             })
             .then((response) => {
-                setSource("http://127.0.0.1:5000/" + response.data)
+                document.getElementById("image").src =
+                    "http://127.0.0.1:5000/static/Images/" + response.data;
+                document.getElementById("image").onload = function () {
+                    axios
+                        .post("http://127.0.0.1:5000/remfromdisk", {
+                            path: response.data,
+                        })
+                        .then((r) => {
+                            
+                        });
+                };
             });
     };
 
